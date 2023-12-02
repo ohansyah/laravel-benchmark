f18c06c6eed94be9095497d9393ea0568ff12049
ohansyah <ohansyah@weekendinc.com>
2023-12-03 01:51:04 +0700

    initial commit

---
+++ src/Benchmark.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ohansyah\LaravelBenchmark;
+
+use Closure;
+use Illuminate\Support\Arr;
+
+class Benchmark
+{
+    /**
+     * Measure a callable or array of callables over the given number of iterations.
+     *
+     * @param  \Closure|array  $benchmarkables
+     * @param  int  $iterations
+     * @return array|float
+     */
+    public static function measure($benchmarkables, int $iterations = 1)
+    {
+        return collect(Arr::wrap($benchmarkables))->map(function ($callback) use ($iterations) {
+            return collect(range(1, $iterations))->map(function () use ($callback) {
+                gc_collect_cycles();
+
+                $start = hrtime(true);
+
+                $callback();
+
+                return (hrtime(true) - $start) / 1000000;
+            })->average();
+        })->when(
+            $benchmarkables instanceof Closure,
+            function ($c) {
+                return $c->first();
+            },
+            function ($c) {
+                return $c->all();
+            },
+        );
+    }
+
+    /**
+     * Measure a callable or array of callables over the given number of iterations, then dump and die.
+     *
+     * @param  \Closure|array  $benchmarkables
+     * @param  int  $iterations
+     * @return never
+     */
+    public static function dd($benchmarkables, int $iterations = 1): void
+    {
+        $result = collect(static::measure(Arr::wrap($benchmarkables), $iterations))
+            ->map(function ($average) {
+                return number_format($average, 3) . 'ms';
+            })
+            ->when(
+                $benchmarkables instanceof Closure,
+                function ($c) {
+                    return $c->first();
+                },
+                function ($c) {
+                    return $c->all();
+                });
+
+        dd($result);
+    }
+}
